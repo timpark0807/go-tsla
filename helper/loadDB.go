7854f1f2906129117e30d3851580832933448763
Tim Park <timpark0807@gmail.com>
2020-06-18 20:46:37 -0700

    Load Database with scraped values

--- helper/loadDB.go
+++ helper/loadDB.go
@@ -1,14 +1,61 @@
 package helper
 
 import (
+	"database/sql"
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
+	"os"
+	"strings"
 
+	_ "github.com/go-sql-driver/mysql"
 	"github.com/timpark0807/go-tsla/model"
 )
 
-func LoadDB(stations model.Stations) {
+func ReadFile() model.Stations {
+	jsonFile, err := os.Open("data.json")
+	var stations model.Stations
 
-	for i := 0; i < len(stations.Stations); i++ {
-		fmt.Println(stations.Stations[i])
+	if err != nil {
+		fmt.Println(err)
+		return stations
 	}
+
+	byteValue, _ := ioutil.ReadAll(jsonFile)
+	json.Unmarshal(byteValue, &stations)
+
+	defer jsonFile.Close()
+	return stations
+}
+
+func LoadDB(stations []model.Station) {
+
+	dbConnectionString, exists := os.LookupEnv("DB_CONNECTION_STRING")
+
+	if !exists {
+		return
+	}
+
+	db, err := sql.Open("mysql", dbConnectionString)
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer db.Close()
+
+	valueStrings := make([]string, 0, len(stations))
+	valueArgs := make([]interface{}, 0, len(stations)*6)
+	for index, station := range stations {
+		valueStrings = append(valueStrings, "(?, ?, ?, ?, ?, ?)")
+		valueArgs = append(valueArgs, index)
+		valueArgs = append(valueArgs, station.Name)
+		valueArgs = append(valueArgs, station.URL)
+		valueArgs = append(valueArgs, station.Active)
+		valueArgs = append(valueArgs, station.Address.Street)
+		valueArgs = append(valueArgs, station.Address.Locality)
+	}
+	stmt := fmt.Sprintf("INSERT INTO superchargers (id, name, url, active, street, locality) VALUES %s",
+		strings.Join(valueStrings, ","))
+	_, err = db.Exec(stmt, valueArgs...)
+	fmt.Println(err)
+
 }
--- main.go
+++ main.go
@@ -1,32 +1,6 @@
 package main
 
-import (
-	"encoding/json"
-	"fmt"
-	"io/ioutil"
-	"os"
-
-	"github.com/timpark0807/go-tsla/helper"
-	"github.com/timpark0807/go-tsla/model"
-)
-
-func readFile() model.Stations {
-	jsonFile, err := os.Open("data.json")
-	var stations model.Stations
-
-	if err != nil {
-		fmt.Println(err)
-		return stations
-	}
-
-	byteValue, _ := ioutil.ReadAll(jsonFile)
-	json.Unmarshal(byteValue, &stations)
-
-	defer jsonFile.Close()
-	return stations
-}
-
 func main() {
-	stations := readFile()
-	helper.LoadDB(stations)
+	// stations := helper.ReadFile()
+	// helper.LoadDB(stations.Stations)
 }
